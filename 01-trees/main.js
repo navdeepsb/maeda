dcf26ded3f6aa8245d3c33bc92b6b788b2c3d8a8
Navdeep Singh Bagga <navdeepb3191@gmail.com>
2018-05-19 09:10:42 -0400

    Added individual background (pngs) for each season

--- 01-trees/main.js
+++ 01-trees/main.js
@@ -9,10 +9,6 @@ window.app = ((()=>{
 
     // ...
     let getRenderedHtml = ( imgSrc, imgMargin, imgAnimation ) => {
-        if( !imgSrc )
-            // Return the background tile:
-            return `<li class="tile" style="width: ${ IMG_WIDTH }px; height: ${ IMG_WIDTH }px; margin-top: ${ imgMargin }px; margin-left: ${ imgMargin }px"></li>`;
-
         return `
             <li class="tile ${ imgAnimation }" style="margin-top: ${ imgMargin }px; margin-left: ${ imgMargin }px">
                 <img src="img/${ imgSrc }" width="${ IMG_WIDTH }" />
@@ -25,7 +21,6 @@ window.app = ((()=>{
         parentElem = parentElem || document.createElement( "ul" );
         parentElem.classList.add( "canvas" );
 
-        parentElem.innerHTML = getRenderedHtml( null, margin ); // for bkg layer
         images.forEach( img => parentElem.innerHTML += getRenderedHtml( img.src, margin, img.anim ) );
 
         document.body.appendChild( parentElem );
